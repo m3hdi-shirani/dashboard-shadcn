774138de71d3d548a31fa333a382c8f1331277aa
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-07-01 10:17:46 +0330

    feat: implement AppBarChart component and integrate it into the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import AppBarChart from "@/components/AppBarChart";
+
 export default function Home() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2">
-        test
+        <AppBarChart />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg">test</div>
       <div className="bg-primary-foreground p-4 rounded-lg">test</div>
