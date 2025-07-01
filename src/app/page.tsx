35d1372c3f5b35ffac7a2b1aa83d167ae7ecb23d
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-07-01 10:01:52 +0330

    feat: add recharts library and implement chart components
    - Added `recharts` dependency to package.json and package-lock.json.
    - Created `Card` component for UI consistency.
    - Implemented `ChartContainer`, `ChartTooltip`, `ChartLegend`, and related components for rendering charts using Recharts.
    - Enhanced the `Home` page layout with a grid structure for better visual organization.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <div className="">Home Page</div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2">
+        test
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2">
+        test
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">test</div>
+    </div>
+  );
 }
